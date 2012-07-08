f5a25ac451c41d30fa86c6052dde4136447b50fa
Patrick Ahlbrecht <github@onyxbits.de>
2012-07-08 14:39:30 +0200

    Completely rewrote the AppRater to use a visually more appealing dialog and to do without inner classes.

--- src/de/onyxbits/sensorreadout/AppRater.java
+++ src/de/onyxbits/sensorreadout/AppRater.java
@@ -11,91 +11,92 @@ import android.content.*;
 import android.net.*;
 
 /**
- * Shamelessly stolen from:
- * http://www.androidsnippets.com/prompt-engaged-users-to-rate-your-app-in-the-android-market-appirater
+ * Bug the user about rating the app after a reasonable amount of uses.
  */
-public class AppRater {
-    private final static String APP_TITLE = "Sensor Readout";
-    private final static String APP_PNAME = "de.onyxbits.sensorreadout";
-    
-    private final static int DAYS_UNTIL_PROMPT = 2;
-    private final static int LAUNCHES_UNTIL_PROMPT = 7;
-    
-    public static void app_launched(Context mContext) {
-        SharedPreferences prefs = mContext.getSharedPreferences("apprater", 0);
-        if (prefs.getBoolean("dontshowagain", false)) { return ; }
-        
-        SharedPreferences.Editor editor = prefs.edit();
-        
-        // Increment launch counter
-        long launch_count = prefs.getLong("launch_count", 0) + 1;
-        editor.putLong("launch_count", launch_count);
+public class AppRater implements DialogInterface.OnClickListener {
 
-        // Get date of first launch
-        Long date_firstLaunch = prefs.getLong("date_firstlaunch", 0);
-        if (date_firstLaunch == 0) {
-            date_firstLaunch = System.currentTimeMillis();
-            editor.putLong("date_firstlaunch", date_firstLaunch);
-        }
-        
-        // Wait at least n days before opening
-        if (launch_count >= LAUNCHES_UNTIL_PROMPT) {
-            if (System.currentTimeMillis() >= date_firstLaunch + 
-                    (DAYS_UNTIL_PROMPT * 24 * 60 * 60 * 1000)) {
-                showRateDialog(mContext, editor);
-            }
-        }
-        
-        editor.commit();
-    }   
-    
-    public static void showRateDialog(final Context mContext, final SharedPreferences.Editor editor) {
-        final Dialog dialog = new Dialog(mContext);
-        dialog.setTitle("Rate " + APP_TITLE+"?");
-
-        LinearLayout ll = new LinearLayout(mContext);
-        ll.setOrientation(LinearLayout.VERTICAL);
-        
-        TextView tv = new TextView(mContext);
-        //tv.setText("If you enjoy using " + APP_TITLE + ", please take a moment to rate it. Thanks for your support!");
-        tv.setText("Hi,\nseems like "+APP_TITLE+" is useful to you. Would you consider rating it? This will only take a minute of your time, but you will encourage further development this way.");
-        tv.setWidth(240);
-        tv.setPadding(4, 0, 4, 10);
-        ll.addView(tv);
-        
-        Button b1 = new Button(mContext);
-        b1.setText("Rate " + APP_TITLE);
-        b1.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                mContext.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + APP_PNAME)));
-                dialog.dismiss();
-            }
-        });        
-        ll.addView(b1);
-
-        Button b2 = new Button(mContext);
-        b2.setText("Remind me later");
-        b2.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                dialog.dismiss();
-            }
-        });
-        ll.addView(b2);
-
-        Button b3 = new Button(mContext);
-        b3.setText("No, thanks");
-        b3.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                if (editor != null) {
-                    editor.putBoolean("dontshowagain", true);
-                    editor.commit();
-                }
-                dialog.dismiss();
-            }
-        });
-        ll.addView(b3);
-
-        dialog.setContentView(ll);        
-        dialog.show();        
+  private final static String APP_PNAME = "de.onyxbits.sensorreadout";
+  
+  private static final int DAYS_UNTIL_PROMPT = 2;
+  private static final int LAUNCHES_UNTIL_PROMPT = 7;
+  private static final String PREFSFILE = "apprater";
+  
+  private Context context;
+  
+  private AppRater(Context context) {
+    this.context=context;
+  }
+  
+  /**
+   * Should be called on every <code>Activity.onCreate()</code>. Checks if the trial
+   * period is over and if so brings up the dialog to rate the app
+   * @param ctx The application context.
+   */
+  public static void appLaunched(Context ctx) {
+    SharedPreferences prefs = ctx.getSharedPreferences(PREFSFILE, 0);
+    if (prefs.getBoolean("dontshowagain", false)) { 
+      return;
     }
+    
+    SharedPreferences.Editor editor = prefs.edit();
+    
+    // Increment launch counter
+    long launch_count = prefs.getLong("launch_count", 0) + 1;
+    editor.putLong("launch_count", launch_count);
+
+    // Get date of first launch
+    Long date_firstLaunch = prefs.getLong("date_firstlaunch", 0);
+    if (date_firstLaunch == 0) {
+      date_firstLaunch = System.currentTimeMillis();
+      editor.putLong("date_firstlaunch", date_firstLaunch);
+    }
+    editor.commit();
+    
+    // Wait at least n days before opening
+    if (launch_count >= LAUNCHES_UNTIL_PROMPT) {
+      if (System.currentTimeMillis() >= date_firstLaunch + (DAYS_UNTIL_PROMPT * 24 * 60 * 60 * 1000)) {
+        showRateDialog(ctx);
+      }
+    }
+  }   
+  
+  /**
+   * Just bring up the rating dialog without checking whether or not the trial perriod has ended
+   * @param ctx application context
+   */
+  public static void showRateDialog(Context ctx) {
+    AppRater rater = new AppRater(ctx);
+    AlertDialog.Builder builder = new AlertDialog.Builder(ctx);
+    builder.setTitle("Rate this app?");
+    builder.setIcon(R.drawable.ic_rate);
+    builder.setMessage("Hi,\nseems like you find this app useful. Would you consider rating it? This will only take a minute of your time, but encourages further development.");
+    builder.setPositiveButton("Sure!",rater);
+    builder.setNegativeButton("No thanks",rater);
+    builder.setNeutralButton("Later",rater);
+    builder.create().show();
+  }
+  
+  @Override
+  public void onClick(DialogInterface di, int id) {
+    SharedPreferences.Editor editor = context.getSharedPreferences(PREFSFILE, 0).edit();
+    switch(id) {
+      case DialogInterface.BUTTON_POSITIVE: {
+        context.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + APP_PNAME)));
+        // Fall through
+      }
+      case DialogInterface.BUTTON_NEGATIVE: {
+        editor.putBoolean("dontshowagain", true);
+        break;
+      }
+      case DialogInterface.BUTTON_NEUTRAL:
+      default: {
+        // Did the user select "Later" because s/he does not have network access right
+        // now or because s/he wants to extend the trial period? Let's assume the later
+        // and reset the launch counter, as it is the less pesky option.
+        editor.putLong("launch_count",0);
+      }
+    }
+    editor.commit();
+    di.dismiss();
+  }
 }
